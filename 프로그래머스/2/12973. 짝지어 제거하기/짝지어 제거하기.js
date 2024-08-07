8df32adcae8146c1cb4544c82c42884322df856d
장희수 <77227728+heesu52@users.noreply.github.com>
2024-08-07 18:48:48 +0900

    [level 2] Title: 짝지어 제거하기, Time: 58.59 ms, Memory: 50 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -2,11 +2,12 @@ function solution(s){
     s = s.split("")
     var answer = [];
     
+    //stack
     for(let i=0 ; i<s.length; i++){
-        if(s[i]===answer[answer.length-1]){
-            answer.pop()
+        if(s[i]===answer[answer.length-1]){ //answer.length-1 = 이전에 answer.push된 객체
+            answer.pop() //같으면 이전 객체 제거
         }else{
-            answer.push(s[i])
+            answer.push(s[i]) // 다르면 추가
         }
     }
     let result = answer.length ===0 ? 1 : 0;
