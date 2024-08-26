13a9cb3b29d5ac9592a6fb163356baacee1650a1
장희수 <77227728+heesu52@users.noreply.github.com>
2024-08-27 00:12:09 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,8 +1,17 @@
 function solution(n) {
-    const dp=[1, 1]; //dp[0]=dp[1]=1
+    const f=[0,1,2];
     
-    for(let i=2; i<=n ; i++){
-        dp[i] = (dp[i-2]+dp[i-1])%1234567
+    for(let i=3; i<=n; i++){
+        f[i] = (f[i-1] + f[i-2]) % 1234567
     }
-    return dp[n]
+    return f[n]
 }
+
+// n=1 -> result=1
+// n=2 -> result=2
+// n=3 -> result=3
+// n=4 -> result=5
+// n=5 -> result=8
+//피보나치 수열 f[n] = f[n-1]+f[n-2]
+
+
