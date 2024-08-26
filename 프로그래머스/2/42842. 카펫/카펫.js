fc651c3a0cb855faaa7c90520f91b03a2237b0f2
장희수 <77227728+heesu52@users.noreply.github.com>
2024-08-26 23:24:05 +0900

    [level 2] Title: 카펫, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,19 +1,15 @@
 function solution(brown, yellow) {
-    var answer = [];
-    let sum = yellow+brown;
+    let sum = brown+yellow;
+    var result=[];
     
-    for(let height=3; height<=brown; height++){ //높이가 최소 3이상
+    for(let height=3; height<=brown; height++){
         if(sum%height===0){
-            let weight = sum / height
+            let width = sum / height //brown + yellow = w*h
             
-        // 공식 : 갈색과 노란색의 합을 임의의 높이로 나눌 때 나오는 높이와 가로 값을 토대로
-     // (가로-2) * (높이-2) = 노란색 격자 수 -> 현재 높이, 가로의 길이
-        if((height-2)*(weight-2) === yellow)
-            return [weight,height]
-        }    
+            if((width-2)*(height-2)===yellow) //(w-2)*(h-2) = yellow
+                return [width, height]
+        }
     }
-    return answer;
+    return result;
 }
-    
-     
 
