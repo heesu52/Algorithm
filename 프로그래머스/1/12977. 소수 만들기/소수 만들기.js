4b7c695854ccd23ac4218342c93339c1feaf06c0
장희수 <77227728+heesu52@users.noreply.github.com>
2024-09-06 17:25:55 +0900

    [level 1] Title: 소수 만들기, Time: 514.30 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -1,20 +1,26 @@
+
+function isPrime(num) {
+  for(let i = 2; num > i; i++) {
+  if(num % i === 0) { //이 부분에서 num이  다른 수로 나눠떨어진다면 소수가 아님
+    return false;
+   }
+  }
+ return num > 1;
+}
+
 function solution(nums) {
-    let answer =0;
+    let answer = 0;
+    let count = 0;
     
-    for(let i=0 ; i<nums.length-2; i++){
-        for(let j=i+1 ; j<nums.length-1; j++){
-            for(let k=j+1 ; k<nums.length; k++){
-                let sum = nums[i]+nums[j]+nums[k];
-                answer++; 
+    for(let i=0; i<nums.length-2; i++){
+        for(let j=i+1; j<nums.length-1; j++){
+            for(let k=j+1; k<nums.length; k++) {
+                answer = nums[i]+nums[j]+nums[k];
                 
-                for (let n = 2; n<=Math.sqrt(sum); n++) {
-                    if (sum % n === 0) {
-                        answer--;
-                        break;
-                    }
-                }
+                  if(isPrime(answer))
+                      count++;
             }
         }
     }
-    return answer;
+    return count;
 }
