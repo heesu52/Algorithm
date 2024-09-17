abda6093d3c931bbf8bfc856a7f76a61da75eb8d
장희수 <77227728+heesu52@users.noreply.github.com>
2024-09-18 02:27:43 +0900

    [level 1] Title: 삼총사, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -0,0 +1,16 @@
+function solution(number) {
+    let answer = 0;
+    let sum = 0;
+    
+    for(let i=0; i<number.length; i++){
+        for(let j=i+1; j<number.length; j++){
+            for(let k=j+1; k<number.length; k++){
+                sum = number[i] + number[j]+ number[k];
+                if(sum === 0){
+                    answer++;
+                }
+            }
+        }
+    }
+    return answer;
+}
