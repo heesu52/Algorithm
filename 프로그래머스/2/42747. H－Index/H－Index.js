3bf0ce37359391051569b470837af5b9189b4685
장희수 <77227728+heesu52@users.noreply.github.com>
2024-10-04 00:19:41 +0900

    [level 2] Title: H-Index, Time: 0.84 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,25 +1,4 @@
 function solution(citations) {
-    citations.sort((a,b)=>b-a);
-    const hIndex = [];
-    
-    for(let i=0; i<citations.length; i++){
-        let a = citations[i];
-        let b = i+1;
-        
-        if(a<b)
-            hIndex.push(a)
-        else
-            hIndex.push(b);
-    }
-    hIndex.sort((a,b)=>b-a);
-    return hIndex[0];
+    var answer = 0;
+    return answer;
 }
-
-//논문 n편 증, a번 이상 인용된 논문이 b편 이상이면 a 와 b중 작은 값이 hIndex 값
-
-//내림차순으로 정렬
-//citations[i]번 이상 인용된 논문은 i+1번 
-//h-index는 citations[i]와 i+1중 작은 값
-// 이 h-index값중 가장 큰 값을 return
-//
-//
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,6 +1,6 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 04일 00:19:06
+2024년 10월 04일 00:19:38
 
 ### 문제 설명
 
