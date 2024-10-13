3e33dd0358cc6e8909d5dabe78e19b2ad8466907
장희수 <77227728+heesu52@users.noreply.github.com>
2024-10-14 00:37:33 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,20 +1,23 @@
+// 지도 1 || 지도 2 벽이면 => 전체에서 벽
+// 지도1 && 지도2 공백이면 => 전체에서도 공백
+// 벽은 1, 공백은 0으로 부호화
+
 function solution(n, arr1, arr2) {
     var answer = [];
     
-    for(let i=0; i<n ; i++){
-        const arr = (arr1[i]|arr2[i]).toString(2);
-        let string=[];
-        //n자리수에 미치지 못할 경우 제일 앞에 0이 있어야함
-        for(let j=arr.length-n; j<arr.length; j++){ //arr[-1]일 경우 " "이 push되도록 해야함
-            if(arr[j]==="1"){
-                string.push("#")
-            }
-            else{
-                string.push(" ")
-            }
-        }
-        answer.push(string.join(""))
+   for(let i=0; i<n; i++) {
+       let binary = (arr1[i] | arr2[i]).toString(2).padStart(n,0);
+       
+       let map = "";
+       for(let j =0; j<n; j++){
+           map += binary[j] === "1" ? "#" : " ";
+       }
+           answer.push(map);
     }
     return answer;
 }
-         
+
+// arr1과 arr2를 각각 이진법으로 전환
+// 둘다 0 이면 "" 그외에는 다 #로 변경
+// arr1과 arr2를 합친 결과를 넣을 answer 배열
+// for문의 반복횟수는 n번
