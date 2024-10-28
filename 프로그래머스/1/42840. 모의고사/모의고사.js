dffb975360a4217b67ea1029228b364aa11ecdcf
장희수 <77227728+heesu52@users.noreply.github.com>
2024-10-28 22:47:21 +0900

    [level 1] Title: 모의고사, Time: 3.62 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,33 +1,27 @@
 function solution(answers) {
-    const patterns = [
-        [1, 2, 3, 4, 5], // 1번 수포자
-        [2, 1, 2, 3, 2, 4, 2, 5], // 2번 수포자
-        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] // 3번 수포자
-    ];
-
+    const math1 = [1,2,3,4,5];
+    const math2 = [2,1,2,3,2,4,2,5];
+    const math3 = [3,3,1,1,2,2,4,4,5,5];
     
-    const scores = [0, 0, 0]; // 각 수포자의 점수를 저장할 배열
-    const result = []; // 가장 높은 점수를 받은 사람을 저장할 배열
+    const count = [0,0,0];
+    for(let i=0; i<answers.length; i++){
+        if(answers[i]===math1[i % math1.length]) count[0]++;
+        if(answers[i]===math2[i % math2.length]) count[1]++;
+        if(answers[i]===math3[i % math3.length]) count[2]++;
+    }
     
-    answers.forEach((answer,index) => { //answers 배열의 요소를 for
-        patterns.forEach((pattern, i) => { //patterns 배열의 요소를 for(1번, 2번, 3번)
-            if(answer === pattern[index % pattern.length]) 
-                scores[i]++;
-        }) //index % pattern.length는 찍는 방식이 반복되는것을 해결
-        //answer = 1, pattern[0%5]=pattern[0]=1 을 비교
-        //맞으면 score에 저장
-    })
-    const Max = Math.max(...scores); //score에서 가장 큰 수 반환
+    const result = [];
+    const max = Math.max(count[0],count[1],count[2]);
     
-    scores.forEach((score,index) => {
-        if(score === Max) //만약score과 최대 득점 수가 동일하다면 
-            result.push(index+1); //result에 해당 점수의 수포자를 push
-    });
-    
+    for(let j=0; j<count.length; j++){
+        if(max===count[j])
+            result.push(j+1);
+    }
     return result;
-    
 }
 
-//1번: 1,2,3,4,5
-//2번: 2,1, 2,3, 2,4, 2,5
-//3번: 33,11,22,44,55 
+// 수포자들의 패턴을 넣을 pattern배열 
+// 맞춘 점수를 count할 count 배열
+// 가장 높은 점수를 받은 사람을 넣을 result 배열
+// answer과 pattern을 반복하여 맞는 문제 수 만큼 count++ -> sccore에 push
+// score의 점수를 비교하고 더 큰 점수의 인덱스를 result에 넣기
