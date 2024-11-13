0bcd97a60775c3647910d64faca31db3d2a4de25
장희수 <77227728+heesu52@users.noreply.github.com>
2024-11-14 01:10:08 +0900

    [level 2] Title: 타겟 넘버, Time: 37.12 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,18 +1,21 @@
 function solution(numbers, target) {
-    let count = 0;
+    let count=0;
     
-    function dfs(index,currentsum){
-        if(index === numbers.length){
-            if(currentsum === target){
-                count ++;
+    dfs(0,0);
+    
+    function dfs(sum, index) {
+        if(index === numbers.length){ //모든 원소를 다 탐색했을 경우
+            if(sum === target){ 
+                count++;
             }
             return;
         }
-        
-        dfs(index+1, currentsum + numbers[index]);
-        dfs(index+1, currentsum - numbers[index]);
-    }
-    dfs(0,0);
-    
+        dfs(sum+numbers[index], index+1); //다음 원소를 더하는 경우
+        dfs(sum-numbers[index], index+1); //다음 원소를 빼는 경우
+    } 
     return count;
 }
+
+//원소를 더하는 경우와 빼는 경우
+//sum이 target과 같은 경우 count++
+//모든 원소의 더하고 빼는 경우를 알아아함 -> dfs
