2a534f86dd974413da009914619fcc5cb91b16e1
장희수 <77227728+heesu52@users.noreply.github.com>
2025-01-18 23:32:02 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 35.55 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,42 +1,56 @@
 function solution(maps) {
-    //상하좌우 설정
-    const dx = [-1,0,1,0];
-    const dy = [0,1,0,-1];
     
-    //최단거리 구하기 = BFS
-    function bfs() {
-        const q = [[0,0,1]]; //[행, 열, 이동거리]
-        maps[0][0] = 0; // 방문한 위치는 0으로 표시(재방문X)
+    const dX = [-1,0,1,0];
+    const dY = [0,-1,0,1];
+    
+    function bfs(){
+        const q = [[0,0,1]];
+        maps[0][0] = 0;
         
         while(q.length){
-            const [curX, curY, distance] = q.shift(); //q에서 방문할 위치 꺼내오기
+            const [curX, curY, distance] = q.shift();
             
-            if(curX==maps.length-1 && curY==maps[0].length-1){ //현재 위치가 상대팀 진영일 경우
-                return distance; //이동거리 반환
+            if(curX === maps.length-1 && curY === maps[0].length-1){
+                return distance;
             }
-            
-            //현재위치를 기준으로 상하좌우 탐색
-            for(let i=0; i<4;i++){
-                const x = curX+dx[i]; 
-                const y = curY+dy[i]; 
-                
-                //x,y이 맵 안에 존재하고(시작점이 0,0이므로 0이상) 아직 방문하지 않은 위치일 경우
-                if(x>-1 && x<maps.length && y>-1 && y<maps[0].length && maps[x][y]===1){
-                    q.push([x,y,distance+1]); //방문한 위치를 q에 넣기
-                    maps[x][y]=0; //방문한 위치 0으로 변경
-                }
+            else{
+                for(let i=0; i<4; i++){
+                    const X = curX + dX[i];
+                    const Y = curY + dY[i];
+                    
+                    if(X>-1 && X<=maps.length-1 && Y>-1 && Y<=maps[0].length-1 && maps[X][Y] === 1){
+                        q.push([X, Y, distance+1]);
+                        maps[X][Y] = 0;
+                    }
+                }  
             }
         }
-        return -1; //만약 상대팀 진영에 도착할 수 없을 때
+        return -1;
     }
     return bfs();
 }
 
-//최단거리 => BFS일 확률이 높음
-//시작위치는 [0,0], 적진 위치는 [length-1, length-1];
-// 현재 위치를 기준으로 상하좌우를 탐색 => 상하좌우 설정 시, X와Y를 따로 해도 되고 배열을 원소로 넣어서 한번에 해도 됨
-// 1이면 방문 가능, 0이면 방문x
-// 방문한 위치는 0으로 변경
-// bfs는 인접한 노드는 모두 방문하며 상하좌우에는 우선순위가 없기때문에 동시에 진행
-// 이미 방문한 위치는 재방문x -> 만약 최단거리의 루트가 아니라면 이미 방문한 노드를 방문해야 함 (동시에 진행되기 떄문에 최단거리로 가는 루트가 이미 방문했을거임)
-// 이런 방식으로 최단거리를 찾을 수 있음
+/*
+모든 경우의 수를 고려해야하지만, 최단거리를 찾아야 함 => bfs
+상대팀에 도달 할 수 없으면 -1 return
+갈 수 있는  곳은 1, 갈 수 없는 곳은 0
+이미 지나온 곳은 0으로 처리
+[0,0]이 시작점, [length-1, length-1]이 상대편 진영
+n과 m이 다를 수도 있음
+*/
+
+/*
+상하좌우 설정
+방문해야하는 칸 배열 q = [[현재x, 현재y, 지난 칸 수]
+지난 노드는 0으로 변경 map[x][y] = 0
+q에서 현재 위치를 꺼내옴
+
+1. 현재 위치가 상대편 진영이라면 return 이동거리
+2. 현재 위치가 상대편 진영x
+2-1. 4번 반복하여 상하좌우 탐색
+2-2. 다음 위치가 맵안에 있고 && 아직 방문하지 않은 노드라면
+2-3. 다음 위치로 이동 && 0으로 변경
+2.4. q에 현재 노드를 추가
+
+
+*/
