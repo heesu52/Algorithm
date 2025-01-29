3734b885a510ceb470ffcd5c937afe18a8862621
장희수 <77227728+heesu52@users.noreply.github.com>
2025-01-29 16:45:20 +0900

    [level 2] Title: 귤 고르기, Time: 81.81 ms, Memory: 65 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,30 +1,32 @@
 function solution(k, tangerine) {
-    let size_count = {}; //인덱스별로 몇번 등장하는지 
+    let count = 0;
+    let sum = 0;
+    const sizes = {};
     
     for(let i=0; i<tangerine.length; i++){
-        let size=tangerine[i]
-        if(size_count[size] !== undefined){ //size_count[size] = key
-            size_count[size] +=1; //value
-        } 
+        if(!sizes[tangerine[i]]){
+            sizes[tangerine[i]] = 1;
+        }
         else{
-            size_count[size] = 1
+            sizes[tangerine[i]] += 1;
         }
     }
-    let size_sort = Object.values(size_count).sort((a,b) => b-a) // key의 value가 큰 순으로 정렬
-    let sum_count=0; //k를 채울 수 있는 크기의 종류 값
-    let result=0; //크기의 종류의 갯수
-    
-    for(let j=0; j<size_sort.length; j++){
-        sum_count +=size_sort[j]
-        result++;
-        if(sum_count >=k)
-            break;
+    let sort_size = Object.values(sizes).sort((a,b) =>b-a);
+    for(let i=0; i<sort_size.length; i++){
+        sum += sort_size[i];
+        count++;
+        if(sum >=k) return count;
     }
-    return result;
 }
 
-//1. tangerine의 객체를 key값으로 두고 동일한 객체가 몇번 씩 나온느지를 value로 지정함 -> 딕셔너리 자료구조
-//2. size_count라는 객체를 선언하고 tangerine배열을 반복해서 key값을 선언함
-//3. size_count의 value값(=Oject.values)을 정렬하여 많이 중복된 순으로 나열
-//4. size_sort의 객체 값(크기의 종류)을 k보다 커지거나 같아질 때까지 더함
-//5. 같거나 커질 경우, 최소의 크기의 종류값이 나오므로 return
+/*
+서로 다른 종류의 수의 최솟값
+해쉬로 풀기
+담는 귤의 종류 = count
+*/
+
+/*
+1.key=귤의 무게, object=귤의 갯수
+2.내림차순으로 정렬해서 갯수가 많은 순
+3.내림차순 순으로 k와 같거나 커질때까지 더하기
+*/
