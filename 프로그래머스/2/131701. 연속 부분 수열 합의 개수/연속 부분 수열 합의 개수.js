2a37c2afb23b45a06e4623ae3a9760e8af30769e
장희수 <77227728+heesu52@users.noreply.github.com>
2025-02-05 01:41:09 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 82.96 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,13 +1,33 @@
 function solution(elements) {
-    let sum_arr = new Set();
-    let n= elements.length
+    let result = new Set;
     
-    for(let i=0; i<n; i++){
-        let sum=0;
-        for(let j=i; j<i+n; j++){
-            sum += elements[j%n] //j로 하면 인덱스 범위를 초과
-            sum_arr.add(sum)
+    for(let i=1; i<=elements.length; i++){
+        let sum = 0;
+        for(let j=0; j<elements.length; j++){
+            if(j===0){
+                for(let k=0; k<i; k++){
+                    sum += elements[k];
+                } 
+            }
+            else{
+                sum -= elements[j-1];
+                sum += elements[(j+i-1) % elements.length]
+            }
+            result.add(sum);
         }
     }
-    return sum_arr.size
+    return result.size;
 }
+
+/*
+원형 수열, 연속하는 수열
+슬라이딩 윈도우 알고리즘 활용
+*/
+
+/*
+값을 저장할 result 배열
+시작 인덱스가 0일 경우 => 모든 합 구하기
+그다음 인덱스 부터 => 첫번째 인덱스의 값을 sum에서 제외, 추가된 인덱스의 값을 sum에 추가
+다 더하고 나면 result.add(sum)
+
+*/
