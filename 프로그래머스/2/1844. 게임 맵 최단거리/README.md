4687cbc597fc922987ae7fc714f1d09e3e3eb1ac
장희수 <77227728+heesu52@users.noreply.github.com>
2025-02-17 21:19:34 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 39.11 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844#qna) 
 
 ### 성능 요약
 
-메모리: 37.6 MB, 시간: 35.55 ms
+메모리: 38.1 MB, 시간: 39.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 18일 23:32:01
+2025년 02월 17일 21:19:32
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,28 +1,26 @@
 function solution(maps) {
-    
-    const dX = [-1,0,1,0];
-    const dY = [0,-1,0,1];
+    let dX = [-1,0,1,0];
+    let dY = [0,-1,0,1];
+    const n = maps.length-1; 
+    const m = maps[0].length-1;
     
     function bfs(){
-        const q = [[0,0,1]];
-        maps[0][0] = 0;
+        let node = [[0,0,1]]; //현재위치 & 지난 노드 수
+        maps[0][0] = 0; //현재위치;
         
-        while(q.length){
-            const [curX, curY, distance] = q.shift();
+        while(node.length){
+            let [curX, curY, answer] = node.shift();
             
-            if(curX === maps.length-1 && curY === maps[0].length-1){
-                return distance;
-            }
-            else{
-                for(let i=0; i<4; i++){
-                    const X = curX + dX[i];
-                    const Y = curY + dY[i];
-                    
-                    if(X>-1 && X<=maps.length-1 && Y>-1 && Y<=maps[0].length-1 && maps[X][Y] === 1){
-                        q.push([X, Y, distance+1]);
-                        maps[X][Y] = 0;
-                    }
-                }  
+            if(curX===n && curY===m) return answer;
+            
+            for(let i=0; i<4; i++){
+                const X = curX+dX[i];
+                const Y = curY+dY[i];
+                
+                if(X>-1 && X<=n && Y>-1 && Y<=m && maps[X][Y] !== 0){
+                    node.push([X,Y,answer+1]);
+                    maps[X][Y] = 0;
+                }
             }
         }
         return -1;
@@ -30,27 +28,22 @@ function solution(maps) {
     return bfs();
 }
 
+
 /*
-모든 경우의 수를 고려해야하지만, 최단거리를 찾아야 함 => bfs
-상대팀에 도달 할 수 없으면 -1 return
-갈 수 있는  곳은 1, 갈 수 없는 곳은 0
-이미 지나온 곳은 0으로 처리
-[0,0]이 시작점, [length-1, length-1]이 상대편 진영
-n과 m이 다를 수도 있음
+n*m크기의 2차원 배열
+처음 캐릭터는 (1,1)에 위치 =>[0][0]
+상하좌우 지정
+모든 경로를 찾고 최솟값을 구해야함 
+상대팀 진영에 도착할 수 없을 경우 -1을 return
 */
 
 /*
-상하좌우 설정
-방문해야하는 칸 배열 q = [[현재x, 현재y, 지난 칸 수]
-지난 노드는 0으로 변경 map[x][y] = 0
-q에서 현재 위치를 꺼내옴
-
-1. 현재 위치가 상대편 진영이라면 return 이동거리
-2. 현재 위치가 상대편 진영x
-2-1. 4번 반복하여 상하좌우 탐색
-2-2. 다음 위치가 맵안에 있고 && 아직 방문하지 않은 노드라면
-2-3. 다음 위치로 이동 && 0으로 변경
-2.4. q에 현재 노드를 추가
-
-
+방문한 노드인지 확인할 배열 visited
+방문한 노드의 갯수는 세는 count
+현재 노드를 반환하는 stack
+1. 상하좌우 중 노드값이 1인 부분만 방문
+2. 이미 방문한 노드인지 확인
+3. 방문한 노드가 아니라면 현재 값에서 이동하고 visited에 추가
+4. 방문한 노드수 count++
+4. 현재 노드가 상대팀 진여에 도착했다면 return count
 */
