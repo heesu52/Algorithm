ac1ef40fc90ef4a55f14fe8efef681ebf600e577
장희수 <77227728+heesu52@users.noreply.github.com>
2025-02-22 23:07:46 +0900

    [level 2] Title: 타겟 넘버, Time: 15.18 ms, Memory: 35.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,31 +1,28 @@
 function solution(numbers, target) {
-    let count=0;
+    let count = 0;
     
     dfs(0,0);
     
-    function dfs(sum, index){
-        if(index===numbers.length){
-            if(sum===target){
+    function dfs(sum, i){
+        if(i === numbers.length){
+            if(sum === target){
                 count++;
             }
-            return;
+            return; //함수 종료
         }
-        
-        dfs(sum+numbers[index], index+1);
-        dfs(sum-numbers[index], index+1);
-        
+        dfs(sum-numbers[i],i+1);
+        dfs(sum+numbers[i],i+1);
     }
     return count;
 }
 
 /*
-더하거나 빼는 모든 경우의 수를 다 해봐야 함 = dfs
+모든 경우의 수를 구해야함 -> dfs
+number를 탐색하다가 target과 같아질 때를 확인
 */
 
 /*
-계산 합과 target이 일치하는 횟수를 세는 변수 count
-dfs(sum, index)
-dfs(sum+다음인덱스, index),dfs(sum-다음인덱스, index)
-sum과 target이 같다면 count++
-모든 원소를 다 돌았다면 return
+1. sum+다음원소, 다음원소+1
+2. 인덱스가 numbers의 길이와 같다면 sum과 target을비교
+3. sum과 target이 같다면 count++
 */
