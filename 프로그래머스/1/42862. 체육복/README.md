b4bdb448625f1b0c4284eb5d00d3c1c412b8157b
장희수 <77227728+heesu52@users.noreply.github.com>
2025-02-27 00:50:04 +0900

    [level 1] Title: 체육복, Time: 0.22 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862#qna) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.19 ms
+메모리: 33.7 MB, 시간: 0.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 30일 16:10:51
+2025년 02월 27일 00:50:02
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,25 +1,34 @@
 function solution(n, lost, reserve) {
+    let filter_lost = lost.filter(i=>!reserve.includes(i)).sort((a,b)=>b-a);
+    let filter_reserve = reserve.filter(i=>!lost.includes(i)).sort((a,b)=>b-a);
     
-    // 여벌을 가져왔는데 분실한 학생을 제거
-    let arrlost = lost.filter(l => !reserve.includes(l)).sort((a,b)=>a-b); 
-    let arrreserve = reserve.filter(r => !lost.includes(r)).sort((a,b)=>a-b); 
+    let answer  = n-filter_lost.length;
     
-    //최소 학생 수 
-    let answer = n-arrlost.length;
-    
-    for(let i=0; i<arrlost.length; i++){
-       for(let j=0; j<arrreserve.length; j++){
-           if(arrlost[i]-1 === arrreserve[j] || arrlost[i]+1 === arrreserve[j]){
-               answer++;
-               arrreserve[j]=-1; // 빌려준 학생 제거
-               break; // 만족했으면 더 돌 필요가 없음
-           }
-       }
+    for(let i=0; i<filter_lost.length; i++){
+        for(let j=0; j<filter_reserve.length; j++){
+            if(filter_lost[i]+1 === filter_reserve[j] || filter_lost[i]-1 === filter_reserve[j]){
+                filter_reserve.splice(j,1);
+                answer++;
+                break;
+            }
+        }
     }
     return answer;
+    
 }
 
-//최소 학생 수 = n-잃어버린 학생 수
-//최대 = n 
-//여벌을 가져왔는데 잃어버린 학생 = lost와 reserve 모두 존재
-//여벌분을 잃어버렸으니 본인의 체육복은 본인이 입어야 하므로 lost와 reserve에서 모두 제거
+
+/*
+체육복은 바로 앞/뒷번호 학생에게만
+최대한 많은 학생이 체육복을 가지고 있도록
+여벌 체육복을 가진 학생이 도둑맞을 수 있음 = 빌려줄수x
+*/
+
+/*
+1. reserve에 있는 학생이 lost에도 있는지 확인 후 제거
+2. lost와 reserve를 오름차순으로 정렬(앞/뒷번호만 줄 수 있기 때문에)
+3. n에서 lost.length만큼 제외
+4. reserve의 인덱스 값이 lost에 앞이나 뒤에 잇을 경우 앞에서부터 차례대로 삭제 & n++
+5. reserve에서 해당 인덱스 삭제
+6. 만약 빌려준 경우, 더 빌릴 필요가 없기 때문에 break;
+*/
